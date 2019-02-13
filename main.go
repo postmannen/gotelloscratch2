17998850a5cd278a18eb58c235419904c2b5de8e
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-02-13 22:32:23 +0100

    added sensor readings

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ type cmdData struct {
 }
 
 var cmdFromScratch chan cmdData
+var flightInfo chan tello.FlightData
+
 var speed int = 100
 
 const (
@@ -26,7 +28,7 @@ const (
 //fromScratch is a HandlerFunc that checks the URL path for commands from Scratch
 // and puts the commands received on a channel to be sent to the Tello drone.
 // The drone sends in the format /command/jobID/measure
-func fromScratch(w http.ResponseWriter, r *http.Request) {
+func (d *droneData) fromScratch(w http.ResponseWriter, r *http.Request) {
 	u := r.RequestURI
 	uSplit := strings.Split(u, "/")
 
@@ -39,13 +41,18 @@ func fromScratch(w http.ResponseWriter, r *http.Request) {
 			//fmt.Printf(" * len was less than 2, case detected, uSplit = %#v\n", uSplit)
 		}
 	} else {
+		fmt.Fprintf(w, "%v %v\n", "battery?", d.drone.GetFlightData().BatteryPercentage)
+		fmt.Fprintf(w, "%v %v\n", "speed?", d.drone.GetFlightData().GroundSpeed)
+		fmt.Fprintf(w, "%v %v\n", "time?", d.drone.GetFlightData().DroneFlyTimeLeft)
+		fmt.Fprintf(w, "%v %v\n", "height?", d.drone.GetFlightData().Height)
+		fmt.Fprintf(w, "%v %v\n", "wind?", d.drone.GetFlightData().WindState)
+		fmt.Fprintf(w, "%v %v\n", "ssid?", d.drone.GetFlightData().SSID)
 
 	}
 }
 
-func handleCommand() {
-	drone := new(tello.Tello)
-	err := drone.ControlConnectDefault()
+func (d *droneData) handleCommand() {
+	err := d.drone.ControlConnectDefault()
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
@@ -64,98 +71,118 @@ func handleCommand() {
 		}
 
 		switch cmd.command {
+		case "disconnect":
+			d.drone.ControlDisconnect()
 		case "takeoff":
 			fmt.Println("takeoff")
 			time.Sleep(250 * time.Millisecond)
-			drone.TakeOff()
-			time.Sleep(3 * time.Second)
+			d.drone.TakeOff()
+			time.Sleep(5 * time.Second)
 			fmt.Println("takeoff timer 7 seconds ok")
 		case "land":
 			time.Sleep(1000 * time.Millisecond) //let the drone stand still before we land
 			fmt.Println("land")
-			drone.Land()
-			drone.ControlDisconnect()
+			d.drone.Land()
+			//d.drone.ControlDisconnect()
 		case "left":
 			fmt.Println("left")
-			drone.Left(speed)
+			d.drone.Left(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.Left(0)
+			d.drone.Left(0)
 		case "right":
 			fmt.Println("right")
-			drone.Right(speed)
+			d.drone.Right(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.Right(0)
+			d.drone.Right(0)
 		case "forward":
 			fmt.Println("forward")
-			drone.Forward(speed)
+			d.drone.Forward(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.Forward(0)
+			d.drone.Forward(0)
 		case "back":
 			fmt.Println("back")
-			drone.Backward(speed)
+			d.drone.Backward(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.Backward(0)
+			d.drone.Backward(0)
 		case "up":
 			fmt.Println("up")
-			drone.Up(speed)
+			d.drone.Up(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.Up(0)
+			d.drone.Up(0)
 		case "down":
 			fmt.Println("down")
-			drone.Down(speed)
+			d.drone.Down(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.Down(0)
+			d.drone.Down(0)
+		case "setspeed":
+			fmt.Println("down")
+			speed = int(num2)
 		case "hover":
 			fmt.Println("hover")
-			drone.Hover()
+			d.drone.Hover()
 			if err != nil {
 				log.Println("autoturn failed: ", err)
 			}
 		case "cw":
 			fmt.Println("rotate clockwise")
-			drone.TurnRight(speed)
+			d.drone.TurnRight(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.TurnRight(0)
+			d.drone.TurnRight(0)
 		case "ccw":
 			fmt.Println("rotate clockwise")
-			drone.TurnLeft(speed)
+			d.drone.TurnLeft(speed)
 			time.Sleep(time.Millisecond * 100)
-			drone.TurnLeft(0)
+			d.drone.TurnLeft(0)
 		case "flip":
 			if cmd.data == "forward" {
-				drone.Flip(tello.FlipForward)
+				d.drone.Flip(tello.FlipForward)
 			}
 			if cmd.data == "backward" {
-				drone.Flip(tello.FlipBackward)
+				d.drone.Flip(tello.FlipBackward)
 			}
 			if cmd.data == "left" {
-				drone.Flip(tello.FlipLeft)
+				d.drone.Flip(tello.FlipLeft)
 			}
 			if cmd.data == "right" {
-				drone.Flip(tello.FlipRight)
+				d.drone.Flip(tello.FlipRight)
 			}
 			if cmd.data == "forwardleft" {
-				drone.Flip(tello.FlipForwardLeft)
+				d.drone.Flip(tello.FlipForwardLeft)
 			}
 			if cmd.data == "forwardright" {
-				drone.Flip(tello.FlipForwardRight)
+				d.drone.Flip(tello.FlipForwardRight)
 			}
 			if cmd.data == "backwarleft" {
-				drone.Flip(tello.FlipBackwardLeft)
+				d.drone.Flip(tello.FlipBackwardLeft)
 			}
 			if cmd.data == "backwardright" {
-				drone.Flip(tello.FlipBackwardRight)
+				d.drone.Flip(tello.FlipBackwardRight)
 			}
+			//case "poll":
+			//	fmt.Println("found a poll request")
+			//	select {
+			//	case flightInfo <- drone.GetFlightData():
+			//	default:
+			//		fmt.Println("FlightData channel full")
+			//	}
 		}
 	}
 }
 
+type droneData struct {
+	drone *tello.Tello
+}
+
 func main() {
+	d := &droneData{
+		drone: new(tello.Tello),
+	}
 	cmdFromScratch = make(chan cmdData, 100)
+	flightInfo = make(chan tello.FlightData)
 
-	go handleCommand()
+	go d.handleCommand()
 
-	http.HandleFunc("/", fromScratch)
+	http.HandleFunc("/", d.fromScratch)
 	http.ListenAndServe(scratchListenHost, nil)
 
 }
